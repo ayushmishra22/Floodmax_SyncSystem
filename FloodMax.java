cce44ecb7126949b6782960ec51dd81f69c6f11e
Ayush Mishra <100124510+ayushmishra22@users.noreply.github.com>
2022-02-27 17:25:20 -0600

    Added start of floodmax algorithm

--- FloodMax.java
+++ FloodMax.java
@@ -1,12 +1,12 @@
 import java.util.*;
 
-public class FloodMax {
+/*public class FloodMax {
     public  static  void main(String args[]) {
         int[] process_id = {12, 18, 58, 56, 55, 54};
-        /*for(int i=0;i<process_id.length;i++){
+        for(int i=0;i<process_id.length;i++){
             Process p=new Process(process_id[i]);
             p.start();
-        }*/
+        }
         FloodMax floodMax=new FloodMax();
         floodMax.createThreads(process_id);
     }
@@ -42,5 +42,46 @@ public class FloodMax {
                     continue;
                 }
         }
+ */       
+public class Floodmax {
+	//Step 1. Send id to neighbors
+	//Step 2. Receive messages from all neighbors
+	//Step 3. Select max id and store it in Process.max_id variable
+	//Step 4. Send the max id to all other neighbors
+	
+	public void begin_floodmax(Process p) {
+		int count_neighbours = p.neighbours.length;
+		for(int i = 0; i<=count_neighbours; i++) {
+			Message msg = new Message(p, p.neighbours[i], "max_uid", ""+p.max_uid);
+			if(p.neighbours[i].msg_rear != p.neighbours[i].message_buffer.length -1) {
+				p.neighbours[i].message_buffer[p.msg_rear+1] = msg;	
+				p.neighbours[i].msg_rear += 1;
+			}
+		}
+		
+		while(p.msg_front != p.msg_rear) {
+			Message received_msg = p.message_buffer[p.msg_front];
+			if(received_msg.type  == "max_uid") {
+				if(p.max_uid >= Integer.parseInt(received_msg.message)) {
+					Message msg = new Message(p,received_msg.sender , "reject", "");
+					msg.receiver.message_buffer[msg.receiver.msg_rear+1] = msg;	
+					msg.receiver.msg_rear += 1;
+				if(p.max_uid < Integer.parseInt(p.message_buffer[p.msg_front].message)) {
+					p.max_uid = Integer.parseInt(p.message_buffer[p.msg_front].message);
+					//send the max id to others
+					for(int i = 0; i < p.neighbours.length; i++) {
+						if(p.neighbours[i] != received_msg.sender) {
+							Message msg_to_send = new Message(p, p.neighbours[i], "max_uid", ""+p.max_uid);
+						}
+					}
+					Message new_msg = new Message(p,p.message_buffer[p.msg_front].sender , "reject", "");
+					msg.receiver.message_buffer[msg.receiver.msg_rear+1] = new_msg;	
+				}
+			}
+		}
+		
+	}
+}
+
     }
 
